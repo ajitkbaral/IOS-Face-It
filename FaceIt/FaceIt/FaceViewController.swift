4b6781c2298a194d285d4fba1769625f93c4d245
Ajit Baral <ajitkbaralofficial@gmail.com>
2017-01-01 18:07:41 +0545

    initial commit

---
+++ FaceIt/FaceIt/FaceViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Ajit Kumar Baral on 12/30/16.
+//  Copyright © 2016 Ajit Kumar Baral. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+    
+
+
+}
+
